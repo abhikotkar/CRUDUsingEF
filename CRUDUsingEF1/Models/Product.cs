6cb811f4a4a8f67bcf9b3fb62eadb1463498c0d6
Abhijeet <Abhijeet@DESKTOP-LQC2102>
2022-07-25 12:33:27 +0530

    Add project files.

---
+++ CRUDUsingEF1/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDUsingEF1.Models
+{
+    [Table("Product")]
+
+    public class Product
+    {
+        
+            [Key]
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+
+    }
+
+
